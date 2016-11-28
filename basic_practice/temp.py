1e68e6a62fb3a2f23d225e5c6ba074d94f28a08f
sangjunnn <sangjun.lee1992@gmail.com>
2016-11-28 23:27:41 +0900

    Add files via upload

--- basic_practice/temp.py
+++ basic_practice/temp.py
@@ -1,5 +1,18 @@
 import turtle as t
 
 t.shape("turtle")
+t.reset()
 
-t.forward(50)
+angle = 91
+
+t.bgcolor("black")
+
+t.color("blue")
+
+t.speed(0)
+
+for x in range(2000):
+    t.forward(x)
+    t.left(angle)
+    
+#t.clear()
